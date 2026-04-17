a261edf9b0d3969310c32202025ddab6996951a9
Mohamed Abdelwahed <marsonal@live.com>
2026-04-17 17:30:06 +0200

    v1

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+window.BIRTH_LOCAL_SUPABASE_CONFIG = window.BIRTH_LOCAL_SUPABASE_CONFIG || {
+  url: "https://chxbvffaurgcnblnogfv.supabase.co",
+  anonKey: "sb_publishable_YzS-RG0h8WOWVv1qtltvZA_G67JN-4z",
+  bucket: "birth-records",
+  recordsFolder: "records",
+  adminEmail: "marsonal2000@gmail.com"
+};
